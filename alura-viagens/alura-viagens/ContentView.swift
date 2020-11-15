9f9b04cabeb0e19f5e30674119372dc04a468f85
MathsPrado <matheusprado55@gmail.com>
2020-11-15 13:40:41 -0300

    Changes
    Add images, class, Json to consume and Adjust the Layout

--- alura-viagens/alura-viagens/ContentView.swift
+++ alura-viagens/alura-viagens/ContentView.swift
@@ -12,22 +12,23 @@ struct ContentView: View {
         GeometryReader { view in
             VStack(){
                 VStack() {
-                    Text("ALURA VIAGENS").bold().foregroundColor(.black).font(.custom("Avenir Black", size: 30)).padding(/*@START_MENU_TOKEN@*/.top, 37.0/*@END_MENU_TOKEN@*/).padding()
+                    Text("ALURA VIAGENS").bold().foregroundColor(.black).font(.custom("Avenir Black", size: 30)).padding().padding()
                     Text("Especial").bold().foregroundColor(.white).font(.custom("Avenir Black", size: 21)).frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity,  alignment: .leading).padding(.leading,30)
-                    Spacer()
                     Text("Brasil!").bold().foregroundColor(.white).font(.custom("Avenir Black", size: 21)).frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity,  alignment: .leading).padding(.leading,30)
                 }
-                .frame(width: view.size.width, height: 190, alignment: .top)
+                .frame(width: view.size.width, height: 220, alignment: .top)
                 .background(Color.gray)
-                HStack{
+                HStack {
                     Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
                         Text("Hoteis")
                             .font(.custom("Avenir Medium", size: 27))
                             .foregroundColor(.white)
+                            .cornerRadius(40)
                 }
                     .frame(width: 120, height: 50)
                     .background(Color.blue)
-                    
+                    .offset(x: 60)
+                    Spacer()
                     Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
                         Text("Pacotes")
                             .font(.custom("Avenir Medium", size: 27))
@@ -35,7 +36,10 @@ struct ContentView: View {
                     }
                     .frame(width: 120, height: 50)
                     .background(Color.orange)
+                    .offset(x: -60)
                 }
+                .offset(y: -25)
+                
                 List {
                     Text("Rio de Janeiro")
                     Text("Ceará")
