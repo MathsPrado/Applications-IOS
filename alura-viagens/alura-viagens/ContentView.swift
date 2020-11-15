b4496acd86c3234d317004b19ac80ec51fbf67ea
MathsPrado <matheusprado55@gmail.com>
2020-11-15 10:00:43 -0300

    Changes
    Add Btn and ajust layout

--- alura-viagens/alura-viagens/ContentView.swift
+++ alura-viagens/alura-viagens/ContentView.swift
@@ -12,15 +12,30 @@ struct ContentView: View {
         GeometryReader { view in
             VStack(){
                 VStack() {
-                    Text("ALURA VIAGENS").bold().foregroundColor(.green).font(.custom("Avenir Black", size: 30)).padding(/*@START_MENU_TOKEN@*/.top, 37.0/*@END_MENU_TOKEN@*/).padding()
+                    Text("ALURA VIAGENS").bold().foregroundColor(.black).font(.custom("Avenir Black", size: 30)).padding(/*@START_MENU_TOKEN@*/.top, 37.0/*@END_MENU_TOKEN@*/).padding()
                     Text("Especial").bold().foregroundColor(.white).font(.custom("Avenir Black", size: 21)).frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity,  alignment: .leading).padding(.leading,30)
                     Spacer()
                     Text("Brasil!").bold().foregroundColor(.white).font(.custom("Avenir Black", size: 21)).frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity,  alignment: .leading).padding(.leading,30)
                 }
                 .frame(width: view.size.width, height: 190, alignment: .top)
-                .background(Color.black)
-                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                
+                .background(Color.gray)
+                HStack{
+                    Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                        Text("Hoteis")
+                            .font(.custom("Avenir Medium", size: 27))
+                            .foregroundColor(.white)
+                }
+                    .frame(width: 120, height: 50)
+                    .background(Color.blue)
+                    
+                    Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                        Text("Pacotes")
+                            .font(.custom("Avenir Medium", size: 27))
+                            .foregroundColor(.white)
+                    }
+                    .frame(width: 120, height: 50)
+                    .background(Color.orange)
+                }
                 List {
                     Text("Rio de Janeiro")
                     Text("Ceará")
@@ -29,6 +44,7 @@ struct ContentView: View {
                 }
             }
         }
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
