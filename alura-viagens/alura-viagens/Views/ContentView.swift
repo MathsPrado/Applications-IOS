c76632aa80d244c23922ad020b8aface9fe81083
MathsPrado <matheusprado55@gmail.com>
2020-11-22 15:02:20 -0300

    Maps
    Add Maps

--- alura-viagens/alura-viagens/Views/ContentView.swift
+++ alura-viagens/alura-viagens/Views/ContentView.swift
@@ -11,16 +11,21 @@ struct ContentView: View {
     
     @Environment(\.horizontalSizeClass) var horizontalSizeClass
     var body: some View {
-        GeometryReader { view in
-            VStack(){
-                headerView()
-                    .frame(width: view.size.width, height: self.horizontalSizeClass == .compact ? 280 : 350, alignment: .top)
-                List(viagens) { viagem in
-                    CelulaViagemViw(viagem: viagem)
+        
+        NavigationView {
+            GeometryReader { view in
+                VStack(){
+                    headerView()
+                        .frame(width: view.size.width, height: self.horizontalSizeClass == .compact ? 280 : 350, alignment: .top)
+                    List(viagens) { viagem in
+                        NavigationLink( destination: MapaView(coordenada: viagem.localizacao).navigationBarTitle("Localização")){
+                        CelulaViagemViw(viagem: viagem)
+                        }
+                    }.navigationBarTitle("")
                 }
             }
-        }
-        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+        }.navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
---
+++ alura-viagens/alura-viagens/Views/MapaView.swift
@@ -0,0 +1,31 @@
+//
+//  MapaView.swift
+//  alura-viagens
+//
+//  Created by Matheus Prado on 22/11/20.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapaView: UIViewRepresentable {
+    
+    var coordenada: CLLocationCoordinate2D
+    
+    func makeUIView(context: Context) -> MKMapView {
+        return MKMapView(frame: .zero)
+    }
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        let region = MKCoordinateRegion(center: coordenada, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+
+struct MapaView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapaView(coordenada: viagens[0].localizacao)
+    }
+}
