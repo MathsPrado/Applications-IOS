a138ba01a99e65cd72c41085ec31db5ef1bd2bb6
MathsPrado <matheusprado55@gmail.com>
2020-12-03 23:48:05 -0300

    Final ajusts
    Ultimo commit seguindo a aula, pois finalizou

--- alura-viagens/alura-viagens/Views/Detalhes/DetalhesViagensView.swift
+++ alura-viagens/alura-viagens/Views/Detalhes/DetalhesViagensView.swift
@@ -19,7 +19,7 @@ struct DetalhesViagensView: View {
         
         GeometryReader { view in
             
-            ScrollView(/*@START_MENU_TOKEN@*/.vertical/*@END_MENU_TOKEN@*/, showsIndicators: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/, content: {
+            ScrollView(/*@START_MENU_TOKEN@*/.vertical/*@END_MENU_TOKEN@*/, showsIndicators: false) {
                 
                 VStack{
                     ZStack(alignment: .top){
@@ -37,94 +37,111 @@ struct DetalhesViagensView: View {
                         .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: .infinity, alignment: .leading)
                     }
                 }
+                
                 VStack{
-                    Text(pacoteDeViagem.titulo.uppercased())
-                        .font(.custom("Avenir Black", size: 26))
-                        .foregroundColor(self.corDaFonte)
-                        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: view.size.width, alignment: .leading)
-                       //.padding()
-                    
-                    Text(pacoteDeViagem.descricao)
-                        .font(.custom("Avenir", size: 16))
-                        .foregroundColor(self.corDaFonte)
-                        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: view.size.width, alignment: .leading)
-                        //.padding()
-                    
-                    
-                    Spacer()
-                        .frame(height: 15)
-                    
-                    Text(self.pacoteDeViagem.detalhes)
-                        .font(.custom("Avenir Black", size: 16))
-                        .foregroundColor(self.corDaFonte)
-                        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: view.size.width, alignment: .leading)
-                        //.padding()
-                    Spacer()
-                        .frame(height: 15)
-                    
-                    HStack{
-                        VStack{
-                            Text("Válido para o periodo de:")
-                                .font(.custom("Avenir", size: 14))
-                                .foregroundColor(self.corDaFonte)
-                                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: view.size.width, alignment: .leading)
-                            
-                            Text(self.pacoteDeViagem.dataValidade)
-                                .font(.custom("Avenir", size: 14))
-                                .foregroundColor(self.corDaFonte)
-                                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: view.size.width, alignment: .leading)
-                        }
-                        VStack{
-                            Text("R$ \(self.pacoteDeViagem.valor)")
-                                .font(.custom("Avenir Black", size: 25))
-                                .foregroundColor(.orange)
-                                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: view.size.width, alignment: .trailing)
-                            
-                            Text("Sem taxas em até 12x")
-                                .font(.custom("Avenir Black", size: 14))
-                                .foregroundColor(self.corDaFonte)
-                                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: view.size.width, alignment: .trailing)
-                        }
-                    }
-                    
-                    Divider()
-                    
                     VStack{
-                        Text("O que está incluso")
-                            .font(.custom("Avenir Black", size: 25))
-                            .foregroundColor(self.corDaFonte)
-                            .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: view.size.width, alignment: .leading)
-                    }
-                    
-                    HStack{
-                        VStack{
-                            Image("icone-apartamento")
-                            Text("Apartamento")
-                                .font(.custom("Avenir", size: 14))
+
+                        Group{
+                            Text(pacoteDeViagem.titulo.uppercased())
+                                .font(.custom("Avenir Black", size: 26))
                                 .foregroundColor(self.corDaFonte)
-                                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: view.size.width, alignment: .center)
+                                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: view.size.width, alignment: .leading)
+                               //.padding()
+                            
+                            Text(pacoteDeViagem.descricao)
+                                .font(.custom("Avenir", size: 16))
+                                .foregroundColor(self.corDaFonte)
+                                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: view.size.width, alignment: .leading)
+                                //.padding()
+                            
+                            Spacer()
+                                .frame(height: 15)
                         }
                         
+                        Text(self.pacoteDeViagem.detalhes)
+                            .font(.custom("Avenir Black", size: 16))
+                            .foregroundColor(self.corDaFonte)
+                            .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: view.size.width, alignment: .leading)
+                            //.padding()
                         Spacer()
                             .frame(height: 15)
                         
-                        VStack{
-                            Image("icone-aviao")
-                            Text("Aviao")
-                                .font(.custom("Avenir", size: 14))
-                                .foregroundColor(self.corDaFonte)
-                                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: view.size.width, alignment: .center)
+                        HStack{
+                            VStack{
+                                Text("Válido para o periodo de:")
+                                    .font(.custom("Avenir", size: 14))
+                                    .foregroundColor(self.corDaFonte)
+                                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: view.size.width, alignment: .leading)
+                                
+                                Text(self.pacoteDeViagem.dataValidade)
+                                    .font(.custom("Avenir", size: 14))
+                                    .foregroundColor(self.corDaFonte)
+                                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: view.size.width, alignment: .leading)
+                            }
+                            VStack{
+                                Text("R$ \(self.pacoteDeViagem.valor)")
+                                    .font(.custom("Avenir Black", size: 25))
+                                    .foregroundColor(.orange)
+                                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: view.size.width, alignment: .trailing)
+                                
+                                Text("Sem taxas em até 12x")
+                                    .font(.custom("Avenir Black", size: 14))
+                                    .foregroundColor(self.corDaFonte)
+                                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: view.size.width, alignment: .trailing)
+                            }
                         }
+                        
+                        Divider()
+                        
+                        VStack{
+                            Text("O que está incluso")
+                                .font(.custom("Avenir Black", size: 25))
+                                .foregroundColor(self.corDaFonte)
+                                .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: view.size.width, alignment: .leading)
+                        }
+                        
+                        HStack{
+                            VStack{
+                                Image("icone-apartamento")
+                                Text("Apartamento")
+                                    .font(.custom("Avenir", size: 14))
+                                    .foregroundColor(self.corDaFonte)
+                                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: view.size.width, alignment: .center)
+                            }
+                            
+                            Spacer()
+                                .frame(height: 15)
+                            
+                            VStack{
+                                Image("icone-aviao")
+                                Text("Aviao")
+                                    .font(.custom("Avenir", size: 14))
+                                    .foregroundColor(self.corDaFonte)
+                                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: view.size.width, alignment: .center)
+                            }
+                        }
+                        .padding(.leading, 40)
+                        .padding(.trailing, 40)
+                        
+                        Divider()
+                        
+                        Button(action: {
+                            self.presentationMode.wrappedValue.dismiss()
+                        }) {
+                          Text("Escolher Opção")
+                            .font(.custom("Avenir Black", size: 16))
+                            .foregroundColor(Color.white)
+                        }
+                        .frame(width: view.size.width-50, height: 45, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/ )
+                        .background(Color(red: 221.0/255.0, green: 58.0/255.0, blue: 126.0/255.0))
+                        
+                        
                     }
-                    .padding(.leading, 40)
-                    .padding(.trailing, 40)
-                    
-                    Divider()
-                }
-                .padding(.leading, 20)
-                .padding(.trailing, 20)
+                    .padding(.leading, 20)
+                    .padding(.trailing, 20)
 
-            })
+                }
+            }
         }
     }
 }
@@ -134,3 +151,4 @@ struct DetalhesViagensView_Previews: PreviewProvider {
         DetalhesViagensView(pacoteDeViagem: pacotesDeViagens[0])
     }
 }
+
